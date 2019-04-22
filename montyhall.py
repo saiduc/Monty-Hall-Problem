1ed3a6d6a0fc1905ac8119c99f38cae5ca46f313
saiduc <saipandian97@gmail.com>
2019-04-22 22:05:29 +0100

    Made two plots to show difference between sticking and switching

--- montyhall.py
+++ montyhall.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class door:
     """
     docstring
     """
+
     def __init__(self, item):
         self.item = item
+
 
 def game(chosen, switch):
     """
@@ -18,7 +21,7 @@ def game(chosen, switch):
     doors = [door_A, door_B, door_C]
 
     if chosen == 1:
-        chosen = door_A 
+        chosen = door_A
     elif chosen == 2:
         chosen = door_B
     elif chosen == 3:
@@ -40,6 +43,7 @@ def game(chosen, switch):
 
     return new_chosen.item
 
+
 def run(n, switch):
     """
     docstring
@@ -47,26 +51,34 @@ def run(n, switch):
     goats = 0
     cars = 0
     for i in range(n):
-        chosen = np.random.choice([1,2,3])
+        chosen = np.random.choice([1, 2, 3])
         if game(chosen, switch) == "goat":
             goats += 1
         if game(chosen, switch) == "car":
             cars += 1
     return [goats, cars]
 
-values = run(100000, True)
-goats_frac = str(values[0]/np.sum(values) * 100) + '%'
-cars_frac = str(values[1]/np.sum(values) * 100) + '%'
-labels = ["goats " + goats_frac, "cars " + cars_frac]
-plt.pie(values)
-plt.legend(labels, loc = "lower left")
+
+# running 1st set
+values_switch = run(100000, True)
+goats_frac_switch = str(round(values_switch[0]/np.sum(values_switch) * 100, 3)) + '%'
+cars_frac_switch = str(round(values_switch[1]/np.sum(values_switch) * 100, 3)) + '%'
+labels_switch = ["goats " + goats_frac_switch, "cars " + cars_frac_switch]
+
+# running 2nd set
+values_stay = run(100000, False)
+goats_frac_stay = str(round(values_stay[0]/np.sum(values_stay) * 100, 3)) + '%'
+cars_frac_stay = str(round(values_stay[1]/np.sum(values_stay) * 100, 3)) + '%'
+labels_stay = ["goats " + goats_frac_stay, "cars " + cars_frac_stay]
+
+# plotting 1st graph
+plt.subplot(211)
+plt.title("If Always Switch:")
+plt.pie(values_switch, labels = labels_switch)
+
+# plotting 2nd graph
+plt.subplot(212)
+plt.title("If Always Stay:")
+plt.pie(values_stay, labels = labels_stay)
+
 plt.show()
-
-
-
-
-
-
-
-
-
